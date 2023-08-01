bb43348ddaf73f36af771f056eb63061f2043abe
Maifee Ul Asad <maifeeulasad@gmail.com>
2023-08-01 22:49:06 +0600

    [ui]: header complete w 4 buttons + searchbar;  - saved items  - messages  - notifications  - profile

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Header as AntdHeader } from 'antd/lib/layout/layout';
 import PageHeader from 'antd/lib/page-header';
+import { Button, Input } from 'antd';
+import {
+  HeartOutlined,
+  MessageOutlined,
+  BellOutlined,
+  UserOutlined,
+  SearchOutlined,
+} from '@ant-design/icons';
 
 const Header = () => (
   <AntdHeader style={{ backgroundColor: 'transparent' }}>
     <PageHeader
-      title="re:manage"
-      subTitle="Manage your ways"
+      title="NFT Cars"
+      extra={[<Input.Search
+        placeholder="Used Volkswagon Golf..."
+        style={{ width: 300 }}
+        enterButton={<SearchOutlined />}
+      />,
+      <div className="w-8" />,
+      <Button type="primary" icon={<HeartOutlined />} />,
+      <Button type="primary" icon={<MessageOutlined />} />,
+      <Button type="primary" icon={<BellOutlined />} />,
+      <Button type="primary" icon={<UserOutlined />} />,
+      ]}
     />
   </AntdHeader>
 );
