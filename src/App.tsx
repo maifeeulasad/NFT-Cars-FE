a4e7a4af0cad337f8cdf34c5d1f83f5a8a37351a
Maifee Ul Asad <maifeeulasad@gmail.com>
2023-08-01 20:21:09 +0600

    [ux]: basic ui skeleton, w HCF from antd;

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-const App = () => <div>app</div>;
+import { CustomLayout } from './layout/CustomLayout';
+import { LazyLanding } from './component/landing/LazyLanding';
 
-export { App };
+const App = () => (
+  <BrowserRouter>
+    <CustomLayout>
+      <Routes>
+        <Route path="/" element={<LazyLanding />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </CustomLayout>
+  </BrowserRouter>
+);
+
+// eslint-disable-next-line import/no-default-export
+export default App;
---
+++ src/layout/content/Content.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Content as AntdContent } from 'antd/lib/layout/layout';
+
+interface IPropsContent {
+  children: any
+}
+
+class Content extends React.Component<IPropsContent> {
+  render() {
+    return (
+      <div style={{ margin: '20px', flex: 'grow' }}>
+        <AntdContent>{this.props.children}</AntdContent>
+      </div>
+    );
+  }
+}
+
+export { Content };
